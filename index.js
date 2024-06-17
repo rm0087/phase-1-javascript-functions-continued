4c752639b6bf00903af2284e5ec4b5e4a337b943
rm0087 <michetti.ray@gmail.com>
2024-06-17 12:15:29 -0400

    stuck on last problem

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+//define "hoisting"
+//hoisting: (javascript's ability to call functions before
+//they appear in the code.)
+saturdayFun("bathe my dog")
+function saturdayFun(foo="roller-skate"){
+    return "This Saturday, I want to "+foo+"!";
+}
+
+mondayWork()
+function mondayWork(foo="go to the office"){
+ return "This Monday, I will "+foo+".";
+}
+
+wrapAdjective()
+function wrapAdjective(){
+    return innerFunction()
+}
+
+function innerFunction(foo="special"){
+    //let result = wrapAdjective('*');
+    //console.log(result)
+    //let emphatic = result("a hard worker");
+    return "You are "+foo+"!";
+}
+
+console.log(innerFunction())
